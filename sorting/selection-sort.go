0abde8429b491509c05b6d419aa5a3232f8835ef
Arsh Sharma <arsh@Arshs-MacBook-Pro.local>
2024-12-03 14:06:02 +0530

    adding selection sort
    Signed-off-by: Arsh Sharma <arshsharma461@gmail.com>

---
+++ sorting/selection-sort.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func sortArray(nums []int) {
+	for i := 0; i < len(nums)-1; i++ {
+		min_index := i
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] < nums[min_index] {
+				min_index = j
+			}
+		}
+		nums[i], nums[min_index] = nums[min_index], nums[i]
+	}
+
+}
+
+func main() {
+	nums := []int{64, 23, 42, 19, 22}
+	fmt.Println("Before Sorting: ", nums)
+	sortArray(nums)
+	fmt.Println("After Sorting: ", nums)
+}
